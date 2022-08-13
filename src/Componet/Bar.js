7930f5be1c9b827c9f727e83a490ee09f233426a
Akp-108 <akp1081@gmail.com>
2022-08-13 17:18:17 +0530

    rsp

--- src/Componet/Bar.js
+++ src/Componet/Bar.js
@@ -15,100 +15,100 @@ const Bar = () => {
         </li>
         <li><a href="/details">About RSP</a>
         <ul>
-            <li><a href="#">Vision</a></li>
-            <li><a href="#">Mission</a></li>
-            <li><a href="#">Objective</a></li>
-            <li><a href="#">R&D Work</a></li>
-            <li><a href="#">Governing Body</a></li>
+            <li><a href="/">Vision</a></li>
+            <li><a href="/">Mission</a></li>
+            <li><a href="/">Objective</a></li>
+            <li><a href="/">R&D Work</a></li>
+            <li><a href="/">Governing Body</a></li>
         </ul>
     </li>
-    <li><a href="#">About Us</a>
+    <li><a href="/">About Us</a>
         <ul>
-            <li><a href="#">Founder</a></li>
-            <li><a href="#">President</a></li>
-            <li><a href="#">Director</a></li>
-            <li><a href="#">Board of Trustees</a></li>
+            <li><a href="/">Founder</a></li>
+            <li><a href="/">President</a></li>
+            <li><a href="/">Director</a></li>
+            <li><a href="/">Board of Trustees</a></li>
 
-            <li><a href="#">Research Committee</a>
+            <li><a href="/">Research Committee</a>
                 <span class="fa fa-plus">+</span>
                     <ul>
-                        <li><a href="#">National</a></li>
-                        <li><a href="#">International</a></li>
+                        <li><a href="/">National</a></li>
+                        <li><a href="/">International</a></li>
                     </ul>
                 
             </li>
-            <li><a href="#">Advisory Committee</a></li>
-            <li><a href="#">State Co-ordinators</a></li>
-            <li><a href="#">Area Co-ordinators</a></li>
+            <li><a href="/">Advisory Committee</a></li>
+            <li><a href="/">State Co-ordinators</a></li>
+            <li><a href="/">Area Co-ordinators</a></li>
         </ul>
     </li>
-    <li><a href="#">Projects</a>
+    <li><a href="/">Projects</a>
     <ul>
-    <li><a href="#">Completed</a></li>
-            <li><a href="#">Current</a></li>
-            <li><a href="#">Upcoming</a></li>
+    <li><a href="/">Completed</a></li>
+            <li><a href="/">Current</a></li>
+            <li><a href="/">Upcoming</a></li>
     </ul>
     </li>
-    <li><a href="#">Join Us</a>
+    <li><a href="/">Join Us</a>
         <ul>
-            <li><a href="#">Research Committee</a>
+            <li><a href="/">Research Committee</a>
                     <ul>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Guidelines</a></li>
-                        <li><a href="#">Join Research Committee</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Guidelines</a></li>
+                        <li><a href="/">Join Research Committee</a></li>
                     </ul>
                 
             </li>
-            <li><a href="#">Advisory Committee</a>
+            <li><a href="/">Advisory Committee</a>
                     <ul>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Guidelines</a></li>
-                        <li><a href="#">Join Advisory Committee</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Guidelines</a></li>
+                        <li><a href="/">Join Advisory Committee</a></li>
                     </ul>
                 
             </li>
-            <li><a href="#">Members</a>
+            <li><a href="/">Members</a>
                     <ul>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Guidelines</a></li>
-                        <li><a href="#">Join as a Member</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Guidelines</a></li>
+                        <li><a href="/">Join as a Member</a></li>
                     </ul>
                 
             </li>
-            <li><a href="#">State Co-ordinator</a>
+            <li><a href="/">State Co-ordinator</a>
                     <ul>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Guidelines</a></li>
-                        <li><a href="#">Join as a State Co-ordinator</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Guidelines</a></li>
+                        <li><a href="/">Join as a State Co-ordinator</a></li>
                     </ul>
             </li>
-            <li><a href="#">Area Co-ordinator</a>
+            <li><a href="/">Area Co-ordinator</a>
                     <ul>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Guidelines</a></li>
-                        <li><a href="#">Join as a Area Co-ordinator</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Guidelines</a></li>
+                        <li><a href="/">Join as a Area Co-ordinator</a></li>
                     </ul>
             </li>
-            <li><a href="#">Volunteer</a>
+            <li><a href="/">Volunteer</a>
                     <ul>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Guidelines</a></li>
-                        <li><a href="#">Join as a Volunteer</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Guidelines</a></li>
+                        <li><a href="/">Join as a Volunteer</a></li>
                     </ul>
             </li>
            
         </ul>
     </li>
-    <li><a href="#">Sponsor</a>
+    <li><a href="/">Sponsor</a>
      <ul>
-        <li><a href="#">Current Sponsors</a></li>
-        <li><a href="#">Sponsors committee</a></li>
-        <li><a href="#">Guidelines</a></li>
-        <li><a href="#">Sponsorship form</a></li>
+        <li><a href="/">Current Sponsors</a></li>
+        <li><a href="/">Sponsors committee</a></li>
+        <li><a href="/">Guidelines</a></li>
+        <li><a href="/">Sponsorship form</a></li>
      </ul>
     </li>
-    <li><a href="#">Donate</a></li>
-    <li><a href="#">Contact Us</a></li>
+    <li><a href="/">Donate</a></li>
+    <li><a href="/">Contact Us</a></li>
     </ul>
    
 </nav>
